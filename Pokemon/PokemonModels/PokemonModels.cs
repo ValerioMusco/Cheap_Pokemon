cd4b22a50627e2fd78941bbe30f01c5932f0adda
Valerio PC Techni <byleadx@hotmail.fr>
2023-06-23 16:31:33 +0200

    Initial Commit

---
+++ Pokemon/PokemonModels/PokemonModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.PokemonModels {
+    public static class PokemonModels {
+
+        public static string bulbizarre = @"";
+
+        public static string herbizarre = @"";
+
+        public static string florizarre = @"";
+
+        
+        
+    }
+}
